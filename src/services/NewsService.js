ec3e4f9a654dae07b236a045646e269a53cf6b7d
Seyit Uludağ <76704724+seyituludag0@users.noreply.github.com>
2022-04-19 10:42:52 +0300

    Update NewsService.js

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -4,7 +4,7 @@ const APIURL = "https://localhost:7157/api/"
 
 export default class NewsService{
     getNews(){
-        return axios.get(APIURL + "News/MynetgetAllNews")
+        return axios.get(APIURL + "News/GetAllNews")
     }
 
     newsTitleSearch(newsTitle){
